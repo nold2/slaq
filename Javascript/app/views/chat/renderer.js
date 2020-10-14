0ee32094b0207ed42d6fab0d0da66d6b2c8d204d
Arnold <sebastianbnec@gmail.com>
2020-10-14 21:55:02 +0700

    Clients binded mechanism working for JS

--- Javascript/app/views/chat/renderer.js
+++ Javascript/app/views/chat/renderer.js
@@ -7,35 +7,45 @@ const chatBox = document.getElementById("chat-box");
 
 const form = document.getElementById("send-chat");
 
+const logout = document.getElementById("logout");
+
 chatWindow.innerText = "It's quite here";
 chatBox.disabled = true;
 
 let socket;
+let parser;
 
 ipcRenderer.on("init", (event, {name, port}) => {
     greetings.innerText = `Hi ${name}`;
     chatBox.disabled = false;
+    logout.innerText = `${port} Change port`
+
+    parser = new window.Parser(name)
 
     socket = new WebSocket(`ws://localhost:${port}`)
-    socket.onopen = (() => {
-        socket.send(`${name}: is on port: ${port}`);
-    });
 
     socket.onmessage = ((event) => {
-        renderChats({chat: event.data, dom: chatWindow})
+        event.data.text().then(result => {
+            renderChats({chat: JSON.parse(result), dom: chatWindow})
+        })
     })
+
+    socket.onclose = () => {
+        ipcRenderer.send("logout")
+    }
 });
 
 const renderChats = ({chat, dom}) => {
     const div = document.createElement("div");
-    div.setAttribute("class", "bubble");
-    div.innerHTML = chat;
+    div.setAttribute("class", "bubble__container");
+    div.innerHTML = `<p><strong class="bubble-name">${chat.user}</strong><small class="bubble-time">${chat.time}</small></strojng></p><p class="buble-message">${chat.content}</p>`;
     dom.appendChild(div);
 };
 
 ipcRenderer.on("load-chats", (event, {content}) => {
     if (socket.readyState === WebSocket.OPEN) {
-        socket.send(content)
+        const buffer = parser.setMessage(content).toJSON().toJSONString().toBuffer()
+        socket.send(buffer)
     }
 });
 
@@ -43,10 +53,16 @@ ipcRenderer.on("load-chats", (event, {content}) => {
 const submit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    ipcRenderer.send("chat-sent", formData.get("chat-box"));
+    const message = formData.get("chat-box")
+    const {json: jsonMessage} = parser.setMessage(message).toJSON()
+    renderChats({chat: jsonMessage, dom: chatWindow})
+    ipcRenderer.send("chat-sent", message);
     form.reset();
 };
 
 form.addEventListener("submit", submit);
 
+logout.addEventListener("click", () => {
+    ipcRenderer.send("logout")
+})
 
